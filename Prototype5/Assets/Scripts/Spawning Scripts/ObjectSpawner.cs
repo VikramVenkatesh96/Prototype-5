7afc1ee0ea7af2562a941c2df3cdcecd0bafe84a
Azhar Siddiqui <ozzie4391@gmail.com>
2019-11-09 19:04:03 -0700

    Added objects movement, spawn, destroy and obj type detection

---
+++ Prototype5/Assets/Scripts/Spawning Scripts/ObjectSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+
+{
+
+    public List<GameObject> objects;
+    public List<GameObject> spawnPos;
+    public float respawnTime;
+    private Vector2 screenBounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+        StartCoroutine(objectsWave());
+    }
+
+    private void spawnObjects()
+    {
+        //select a random gameobject to spawn
+        GameObject objectToSpawn = objects[Random.Range(0, objects.Count)];
+        objectToSpawn = Instantiate(objectToSpawn) as GameObject;
+
+        //spawn the object on the right side 
+        //objectToSpawn.transform.position = new Vector2(screenBounds.x * -2, spawnPos[Random.Range(0, spawnPos.Count - 1)].transform.position.y);
+        objectToSpawn.transform.position = new Vector2(spawnPos[0].transform.position.x, spawnPos[Random.Range(0, spawnPos.Count)].transform.position.y);
+    }
+
+    IEnumerator objectsWave()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnObjects();
+        }
+        
+    }
+
+}
---
+++ Prototype5/Assets/Scripts/UI Scripts/Death_counter_manager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Death_counter_manager : MonoBehaviour
+{
+    public float Fill_amount;
+    public float Fill_decreamentor;
+    public float Fill_incrementor;
+
+    public Image Death_bar_outline_image;
+    public Image Death_bar_filler_image;
+    public Text Death_counter_filled_text;
+
+    void Start()
+    {
+        Death_bar_filler_image.fillAmount = 0.0f;
+
+        Fill_amount = 0.1f;
+        Fill_incrementor = 0.1f;
+        Fill_decreamentor = -0.1f;
+
+        Death_counter_filled_text.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        Death_count_manager_script();
+
+        All_degug_here();
+    }
+
+    //inputs for temporary usage
+    void Death_count_manager_script()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Death_bar_filler_image.fillAmount = Fill_amount + Fill_incrementor;
+            Fill_amount = Death_bar_filler_image.fillAmount;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+            Death_bar_filler_image.fillAmount = Fill_amount + Fill_decreamentor;
+            Fill_amount = Death_bar_filler_image.fillAmount;
+        }
+    }
+
+    void All_degug_here()
+    {
+        //Debug.Log(Death_bar_filler_image.fillAmount);
+    }
+}
