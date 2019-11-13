31abbda7a4817628028993f6f330ae0756df7697
YozoraGu <skyline9474@gmail.com>
2019-11-12 21:58:55 -0700

    water sound add, replacing the animation

--- Prototype5/Assets/Scripts/Input/Hose.cs
+++ Prototype5/Assets/Scripts/Input/Hose.cs
@@ -6,11 +6,13 @@ public class Hose : MonoBehaviour
 {
     private BoxCollider2D water;
     private Animator waterAnimator;
+    private AudioSource audioSource;
     void Start()
     {
         water = gameObject.transform.GetChild(0).gameObject.GetComponent<BoxCollider2D>();
         water.enabled = false;
         waterAnimator = transform.GetChild(0).gameObject.GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -22,10 +24,12 @@ public class Hose : MonoBehaviour
     {
         water.enabled = true;
         waterAnimator.SetBool("waterActive", true);
+        audioSource.Play();
     }
     void OnMouseUp()
     {
         water.enabled = false;
         waterAnimator.SetBool("waterActive", false);
+        audioSource.Stop();
     }
 }
