f34825e9a46654e8db151af31d9c46659ebe62b3
Azhar Siddiqui <ozzie4391@gmail.com>
2019-11-13 17:43:55 -0700

    Added pseudocode for explosion in WaterOnExtinguished script

--- Prototype5/Assets/Scripts/Object Scripts/Extinguishing Scripts/WaterOnExtinguished.cs
+++ Prototype5/Assets/Scripts/Object Scripts/Extinguishing Scripts/WaterOnExtinguished.cs
@@ -8,11 +8,12 @@ public class WaterOnExtinguished : MonoBehaviour
     ObjectMovementLeft accelerationLeft;
     //to decelrate it in right direction decrease accelerationRight
     ObjectMovementRight accelerationRight;
+    //public GameObject explosion;
     private Animator animator;
 
     private void Start()
     {
-        animator =GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -32,9 +33,25 @@ public class WaterOnExtinguished : MonoBehaviour
         if (collider.gameObject.tag == "Extinguished" || collider.gameObject.tag == "Burning")
         {
             Debug.Log("collide");
+
+            //instread of setting the extnguished prefab's animator.setBool to true, make an explosion prefab and set its animator.SetBool to true right here
+            //(destroy) the explosion prefab after a few seconds. Something like this:
+            //
+            //explosion.GetComponent<Animator>().SetBool("collide", true);
+            //Destroy(gameObject.transform.parent.gameObject);
+
+            //StartCoroutine(DestroyExplosion());
+
             animator.SetBool("collide", true);
             //Destroy(gameObject.transform.parent.gameObject);
             Destroy(collider.gameObject.transform.parent.gameObject);
         }
     }
+
+    //IEnumerator DestroyExplosion()
+    //{
+    //    yield return new WaitForSeconds(2f);
+    //    Destroy(explosion);
+
+    //}
 }
